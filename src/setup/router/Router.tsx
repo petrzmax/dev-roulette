aa33b9014225c3b1d18c64e7c445b6357693c723
Artur Petrzak <petrzmax@gmail.com>
2022-09-24 15:49:40 +0200

    Add draft Bots page

--- src/setup/router/Router.tsx
+++ src/setup/router/Router.tsx
@@ -1,5 +1,6 @@
 import { useRoutes } from "react-router-dom";
 import About from "../../pages/about/About";
+import Bots from "../../pages/bots/Bots";
 import History from "../../pages/history/History";
 import Home from "../../pages/home/Home";
 import Ranking from "../../pages/ranking/Ranking";
@@ -9,6 +10,7 @@ import RedirectToLandingPage from "./RedirectToLandingPage";
 export default function Router() {
   return useRoutes([
     { path: "/", element: Home() },
+    { path: "/bots", element: Bots() },
     { path: "/history", element: History() },
     { path: "/ranking", element: Ranking() },
     { path: "/stats", element: Stats() },
