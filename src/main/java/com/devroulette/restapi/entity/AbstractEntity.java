3c330e10c3ce590c27bd491b9520927c2991608a
Artur Petrzak <petrzmax@gmail.com>
2022-12-03 19:52:09 +0100

    Add entity Id getter

--- src/main/java/com/devroulette/restapi/entity/AbstractEntity.java
+++ src/main/java/com/devroulette/restapi/entity/AbstractEntity.java
@@ -1,12 +1,16 @@
 package com.devroulette.restapi.entity;
 
+import lombok.Getter;
+
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+@Getter
 @MappedSuperclass
 public abstract class AbstractEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 }
