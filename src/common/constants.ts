401dbf1dc05ae2ef237c2c8bf268ea2c6a671a02
Artur Petrzak <petrzmax@gmail.com>
2022-12-04 16:54:06 +0100

    Handle adding bearer token in request header

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,8 @@
 export const CONTAINER_REPETITIONS = 5;
 export const TILE_SIZE = 5; // (em) TODO - make it available for css
 
+export const ACCESS_TOKEN_COOKIE_NAME = 'access_token';
+
 export enum BetType {
   RED,
   BLACK,
--- src/redux/sagas/requests.ts
+++ src/redux/sagas/requests.ts
@@ -1,10 +1,13 @@
-import axios from 'axios';
+import axios, { AxiosRequestHeaders } from 'axios';
+import Cookies from 'universal-cookie';
+import { ACCESS_TOKEN_COOKIE_NAME } from '../../common/constants';
 import { rollHistoryUrl, rouletteBetUrl, rouletteUrl, sessionUrl } from '../../common/endpoints';
 import { BetDto } from './../reducers/rouletteReducer';
 
 export function requestFetchSession() {
   return axios.request({
     method: 'get',
+    headers: getAuthenticatedHeader(),
     url: sessionUrl
   });
 }
@@ -12,6 +15,7 @@ export function requestFetchSession() {
 export function requestFetchRouletteState() {
   return axios.request({
     method: 'get',
+    headers: headers,
     url: rouletteUrl
   });
 }
@@ -19,6 +23,7 @@ export function requestFetchRouletteState() {
 export function requestPostRouletteBet(betDto: BetDto) {
   return axios.request({
     method: 'post',
+    headers: getAuthenticatedHeader(),
     url: rouletteBetUrl,
     data: betDto
   });
@@ -27,6 +32,18 @@ export function requestPostRouletteBet(betDto: BetDto) {
 export function requestFetchRollHistory() {
   return axios.request({
     method: 'get',
+    headers: headers,
     url: rollHistoryUrl
   });
+}
+
+const headers: AxiosRequestHeaders = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*'
+};
+
+function getAuthenticatedHeader(): AxiosRequestHeaders {
+  const cookies = new Cookies();
+  const token = cookies.get(ACCESS_TOKEN_COOKIE_NAME);
+  return token ? { ...headers, Authorization: 'Bearer ' + token } : headers;
 }
