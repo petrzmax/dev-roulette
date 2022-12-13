815d310143fed0646fb3b0593f7ddf3f440612cb
Artur Petrzak <petrzmax@gmail.com>
2022-12-13 23:08:31 +0100

    Fetch roulette data while initializing app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,19 +7,24 @@ import Cookies from 'universal-cookie';
 import './App.css';
 import Menu from './common/components/menu/Menu';
 import { ACCESS_TOKEN_COOKIE_NAME } from './common/constants';
-import { setIsUserLoggedIn } from './redux/actions/sessionActions';
+import { fetchRouletteState } from './redux/actions/rouletteActions';
+import { fetchSession, setIsUserLoggedIn } from './redux/actions/sessionActions';
 import { useAppDispatch } from './redux/store';
 import Router from './setup/router/Router';
 
 function App() {
   const dispatch = useAppDispatch();
 
-  useEffect(initialize, []);
+  useEffect(initializeApp, []);
 
-  function initialize(): void {
+  function initializeApp(): void {
     const cookies = new Cookies();
+
+    dispatch(fetchRouletteState());
     if (cookies.get(ACCESS_TOKEN_COOKIE_NAME)) {
+      // TODO Validate if token is valid, before setting loggedIn flag.
       dispatch(setIsUserLoggedIn(true));
+      dispatch(fetchSession());
     }
   }
 
--- front-end/src/pages/roulette/Roulette.tsx
+++ front-end/src/pages/roulette/Roulette.tsx
@@ -1,13 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Col, Container, Row, Spinner } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { BetType } from '../../common/constants';
 import { isEmpty } from '../../common/utils/rouletteUtils/typeScriptUtils';
-import { fetchRouletteState } from '../../redux/actions/rouletteActions';
-import { fetchSession } from '../../redux/actions/sessionActions';
 import { selectRollHistory } from '../../redux/reducers/rouletteReducer';
-import { selectIsLoggedIn } from '../../redux/reducers/sessionReducer';
-import { useAppDispatch } from '../../redux/store';
 import BetAmountPanel from './components/betAmountPanel/BetAmountPanel';
 import HistoryBar from './components/historyBar/HistoryBar';
 import ManualBetPanel from './components/manualBetPanel/ManualBetPanel';
@@ -16,17 +12,9 @@ import Wheel from './components/wheel/Wheel';
 import { BetContext } from './context/BetContext';
 
 export default function Roulette() {
-  const dispatch = useAppDispatch();
   const [betAmount, setBetAmount] = useState(0);
 
   const rollHistory = useSelector(selectRollHistory);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  useEffect(() => {
-    // TODO Not working, it's checked before state is changed
-    if (isLoggedIn) dispatch(fetchSession());
-    dispatch(fetchRouletteState());
-  }, []);
 
   return isEmpty(rollHistory) ? (
     // TODO: Wyśrodkować
