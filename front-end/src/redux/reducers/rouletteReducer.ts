80a26e09eed1b8b88ffefd9b9da7cef38e28939c
Artur Petrzak <petrzmax@gmail.com>
2022-12-13 22:00:00 +0100

    Optimize selectLastRoll selector

--- front-end/src/redux/reducers/rouletteReducer.ts
+++ front-end/src/redux/reducers/rouletteReducer.ts
@@ -41,7 +41,8 @@ export interface BetDto {
 
 // Selectors
 export const selectRollHistory = (state: RootState) => state.roulette.rollHistory;
-export const selectLastRoll = (state: RootState) => state.roulette.rollHistory.slice(-1)[0];
+export const selectLastRoll = (state: RootState) =>
+  state.roulette.rollHistory[state.roulette.rollHistory.length - 1];
 export const selectNextRollTimeStamp = (state: RootState) => state.roulette.nextRollTimeStamp;
 export const selectTileCoverageFactor = (state: RootState) => state.roulette.tileCoverageFactor;
 
