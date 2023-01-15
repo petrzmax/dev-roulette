8e0b779e57a430cf6c0af9d2ef2531ad1d9736e7
Artur Petrzak <petrzmax@gmail.com>
2023-01-15 19:58:35 +0100

    Validate bet creation

--- src/main/java/com/devroulette/restapi/constant/ErrorMessages.java
+++ src/main/java/com/devroulette/restapi/constant/ErrorMessages.java
@@ -5,6 +5,7 @@ public class ErrorMessages {
     public static final String NOT_ENOUGH_BALANCE = "Not enough balance";
     public static final String ROLL_NOT_ASSIGNED = "This bet has no Roll assigned!";
     public static final String NUMBER_OUT_OF_ROULETTE_RANGE = "The number is out of roulette range!";
+    public static final String FIELD_IS_NULL = "Field is null";
 
     private ErrorMessages() {
     }
--- src/main/java/com/devroulette/restapi/entity/Bet.java
+++ src/main/java/com/devroulette/restapi/entity/Bet.java
@@ -2,19 +2,21 @@ package com.devroulette.restapi.entity;
 
 import com.devroulette.restapi.constant.BetType;
 import com.devroulette.restapi.constant.ErrorMessages;
+import com.mysema.commons.lang.Assert;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.lang.NonNull;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@RequiredArgsConstructor
 @Table(name = "BETS")
 public class Bet extends AbstractEntity {
     @NonNull
     @Enumerated(EnumType.STRING) // It will consume much more db memory. Use only for testing
     private BetType betType;
-    @NonNull
     private long amount;
 
     @NonNull
@@ -30,6 +32,16 @@ public class Bet extends AbstractEntity {
     @OneToOne
     @JoinColumn(name = "roll_id")
     private Roll roll;
+
+    public Bet(BetType betType, long amount, User user) {
+        Assert.isTrue(betType != null, ErrorMessages.FIELD_IS_NULL);
+        Assert.isTrue(amount > 0, ErrorMessages.NEGATIVE_AMOUNT);
+        Assert.isTrue(user != null, ErrorMessages.FIELD_IS_NULL);
+
+        this.betType = betType;
+        this.amount = amount;
+        this.user = user;
+    }
 
     public boolean isVictory() {
         if (this.roll != null) {
