de21f4ca7ebd79f89e4eff83081496248f63f720
Artur Petrzak <petrzmax@gmail.com>
2023-01-21 00:24:28 +0100

    Use Logger instead of System.out

--- src/main/java/com/devroulette/restapi/factory/RollFactory.java
+++ src/main/java/com/devroulette/restapi/factory/RollFactory.java
@@ -2,12 +2,16 @@ package com.devroulette.restapi.factory;
 
 import com.devroulette.restapi.entity.Roll;
 import com.devroulette.restapi.utils.RouletteUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
 @Component
 public class RollFactory {
+    private static final Logger LOG = LoggerFactory.getLogger(RollFactory.class);
+
     public Roll createRandomRoll() {
         long currentTimeSeed = System.currentTimeMillis();
         Random generator = new Random(currentTimeSeed);
@@ -15,8 +19,7 @@ public class RollFactory {
         int rollResult = generator.nextInt(0, RouletteUtils.ROULETTE_NUMBER_SEQUENCE.size());
         float tileCoverageFactor = generator.nextFloat();
 
-        // TODO logging?
-        System.out.println("Rolled: " + rollResult + " tileCoverage: " + tileCoverageFactor);
+        LOG.info("Rolled: " + rollResult + " tileCoverage: " + tileCoverageFactor);
 
         // TODO make it use real seed
         return new Roll(String.valueOf(currentTimeSeed), rollResult, tileCoverageFactor);
--- src/main/java/com/devroulette/restapi/restController/AdminPanelRestController.java
+++ src/main/java/com/devroulette/restapi/restController/AdminPanelRestController.java
@@ -4,6 +4,8 @@ import com.devroulette.restapi.constant.Endpoints;
 import com.devroulette.restapi.constant.RouletteWorkflowState;
 import com.devroulette.restapi.service.RouletteService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,19 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(Endpoints.ADMIN)
 @RequiredArgsConstructor
 public class AdminPanelRestController {
+    private static final Logger LOG = LoggerFactory.getLogger(AdminPanelRestController.class);
+
     private final RouletteService rouletteService;
 
     @PostMapping("start")
     public ResponseEntity start() {
         this.rouletteService.setState(RouletteWorkflowState.ROLLING);
-        System.out.println("Roulette started!");
+        LOG.info("Roulette started!");
         return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
     @PostMapping("stop")
     public ResponseEntity stop() {
         this.rouletteService.setState(RouletteWorkflowState.STOPPED);
-        System.out.println("Roulette stopped!");
+        LOG.info("Roulette stopped!");
         return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 }
