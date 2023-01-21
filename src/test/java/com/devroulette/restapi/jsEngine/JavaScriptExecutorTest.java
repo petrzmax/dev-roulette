d6d1839daf04ecfb620cdfd0e33586dab922d085
Artur Petrzak <petrzmax@gmail.com>
2023-01-21 15:42:20 +0100

    Add basic JavaScript executor

---
+++ src/test/java/com/devroulette/restapi/jsEngine/JavaScriptExecutorTest.java
@@ -0,0 +1,51 @@
+package com.devroulette.restapi.jsEngine;
+
+import org.graalvm.polyglot.Value;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JavaScriptExecutorTest {
+
+    private final static String VALID_SCRIPT = """
+            function test() {
+                return 10 + 10;
+            }
+                        
+            test();
+            """;
+    private final static String INVALID_SCRIPT = """
+            function test() {
+                returnn 10 + 10;
+            }
+            """;
+
+    private JavaScriptExecutor javaScriptExecutor = new JavaScriptExecutor();
+
+    @Test
+    void isScriptSyntaxCorrectShouldReturnTrueForValidScript() {
+        // when
+        boolean result = this.javaScriptExecutor.isScriptSyntaxCorrect(VALID_SCRIPT);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void isScriptSyntaxCorrectShouldReturnFalseForInvalidScript() {
+        // when
+        boolean result = this.javaScriptExecutor.isScriptSyntaxCorrect(INVALID_SCRIPT);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void executeShouldReturnValueCalculatedInPassedScript() {
+        // when
+        Value result = this.javaScriptExecutor.execute(VALID_SCRIPT);
+
+        // then
+        assertThat(result.asInt()).isEqualTo(20);
+    }
+}
