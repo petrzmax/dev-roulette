27970645e57d3b978d707a6d947e34e17c7ee6c7
Artur Petrzak <petrzmax@gmail.com>
2023-01-22 21:01:51 +0100

    Add bot placing bet & bot bet processing method

--- src/main/java/com/devroulette/restapi/service/BetService.java
+++ src/main/java/com/devroulette/restapi/service/BetService.java
@@ -1,9 +1,10 @@
 package com.devroulette.restapi.service;
 
+import com.devroulette.restapi.constant.BetType;
 import com.devroulette.restapi.dto.BetDto;
-import com.devroulette.restapi.entity.Roll;
-import com.devroulette.restapi.entity.User;
-import com.devroulette.restapi.entity.UserBet;
+import com.devroulette.restapi.entity.*;
+import com.devroulette.restapi.repository.BotBetRepository;
+import com.devroulette.restapi.repository.BotRepository;
 import com.devroulette.restapi.repository.UserBetRepository;
 import com.devroulette.restapi.repository.UserRepository;
 import com.devroulette.restapi.service.query.UserBetQueryService;
@@ -21,7 +22,9 @@ public class BetService {
     private static final Logger LOG = LoggerFactory.getLogger(BetService.class);
 
     private final UserBetRepository userBetRepository;
+    private final BotBetRepository botBetRepository;
     private final UserRepository userRepository;
+    private final BotRepository botRepository;
     private final UserBetQueryService userBetQueryService;
     private final AuthenticatedUserService authorizedUserService;
 
@@ -40,8 +43,18 @@ public class BetService {
                 currentUser.getId(), betDto.amount(), betDto.betType()));
     }
 
-    // TODO probably move to some BetProcessor class
-    public void processBets(Roll roll) {
+    public void bet(Bot bot, long amount, BetType betType) {
+        bot.pay(amount);
+        BotBet bet = new BotBet(betType, amount, bot);
+        this.botBetRepository.save(bet);
+        this.botRepository.save(bot);
+
+        LOG.info(String.format("Bet created by Bot with ID: %1$d. Amount: %2$d, type: %3$s",
+                bot.getId(), amount, betType));
+    }
+
+    // TODO move to BetProcessor class
+    public void processUserBets(Roll roll) {
         List<UserBet> betsToProcess = this.userBetQueryService.getAllNotProcessedBets();
         List<User> updatedUsers = new ArrayList<>();
 
@@ -57,5 +70,24 @@ public class BetService {
 
         this.userRepository.saveAll(updatedUsers);
         this.userBetRepository.saveAll(betsToProcess);
+    }
+
+    // TODO move to BetProcessor class
+    public void processBotBets(Roll roll) {
+        Iterable<BotBet> betsToProcess = this.botBetRepository.findAllByRollIsNull();
+        List<Bot> updatedBots = new ArrayList<>();
+
+        betsToProcess.forEach(bet -> {
+            bet.setRoll(roll);
+
+            if (bet.isVictory()) {
+                Bot bot = bet.getBot();
+                bot.transfer(bet.getPrize());
+                updatedBots.add(bot);
+            }
+        });
+
+        this.botRepository.saveAll(updatedBots);
+        this.botBetRepository.saveAll(betsToProcess);
     }
 }
