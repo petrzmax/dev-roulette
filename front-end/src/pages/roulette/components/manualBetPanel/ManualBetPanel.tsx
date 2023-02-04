8ad5cdd6bdf436fa2848bf689b7ab7bfc2bbe278
Artur Petrzak <petrzmax@gmail.com>
2023-02-04 20:36:29 +0100

    Use better suiting colors for bet panel button

--- front-end/src/pages/roulette/components/manualBetPanel/ManualBetPanel.tsx
+++ front-end/src/pages/roulette/components/manualBetPanel/ManualBetPanel.tsx
@@ -13,9 +13,26 @@ export default function ManualBetPanel(props: ManualBetPanelProps) {
     dispatch(postRouletteBet({ amount: betAmount, betType: props.betType }));
   }
 
+  function getVariant(): string {
+    switch (props.betType) {
+      case BetType.GREEN: {
+        return 'success';
+      }
+      case BetType.RED: {
+        return 'danger';
+      }
+      case BetType.BLACK: {
+        return 'dark';
+      }
+      default: {
+        return 'warning';
+      }
+    }
+  }
+
   return (
     <Card bg="light">
-      <Button variant="danger" className="mx-3 my-2" onClick={makeBet}>
+      <Button variant={getVariant()} className="mx-3 my-2" onClick={makeBet}>
         {props.text}
       </Button>
     </Card>
