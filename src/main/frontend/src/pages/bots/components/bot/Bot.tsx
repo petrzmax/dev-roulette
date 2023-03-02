ed8d2fdcbf169b78eee7bb1ef37bd5b6b79601a5
Artur Petrzak <petrzmax@gmail.com>
2023-03-03 00:24:51 +0100

    Style bot icon & handle modal

--- src/main/frontend/src/pages/bots/components/bot/Bot.tsx
+++ src/main/frontend/src/pages/bots/components/bot/Bot.tsx
@@ -1,24 +1,42 @@
-import { FormCheck } from 'react-bootstrap';
-import { MdDelete, MdEditNote, MdPlayArrow } from 'react-icons/md';
+import { useState } from 'react';
+import { Badge } from 'react-bootstrap';
+import { MdDelete, MdEditNote, MdPlayArrow, MdStop } from 'react-icons/md';
 import { BotDto } from '../../../../redux/user/user.model';
+import css from './Bot.module.scss';
+import ScriptEditorModal from './components/scriptEditModal/ScriptEditorModal';
 
 export default function Bot(props: BotProps) {
+  const [showScriptModal, setShowScriptModal] = useState(false);
+  const handleScriptShow = () => setShowScriptModal(true);
+  const handleScriptClose = () => setShowScriptModal(false);
+
   return (
-    <tr>
-      <td>
-        <FormCheck />
-      </td>
-      <td>{props.bot.name}</td>
-      <td>{props.bot.balance}</td>
-      <td>
-        <MdPlayArrow />
-        <MdEditNote />
-        <MdDelete />
-      </td>
-    </tr>
+    <>
+      <tr>
+        <td>{props.index + 1}</td>
+        <td>{props.bot.name}</td>
+        <td>{props.bot.balance}</td>
+        <td>
+          <Badge bg="success">Stable</Badge>
+        </td>
+        <td>
+          <MdPlayArrow className={css.start} title="Start bot processing" />
+          <MdStop className={css.stop} title="Stop bot processing" />
+          <MdEditNote className={css.edit} title="Edit bot script" onClick={handleScriptShow} />
+          <MdDelete className={css.delete} title="Delete bot" />
+        </td>
+      </tr>
+      <ScriptEditorModal
+        name={props.bot.name}
+        handleClose={handleScriptClose}
+        show={showScriptModal}
+        scriptBody={props.bot.scriptBody}
+      />
+    </>
   );
 }
 
 interface BotProps {
+  index: number;
   bot: BotDto;
 }
