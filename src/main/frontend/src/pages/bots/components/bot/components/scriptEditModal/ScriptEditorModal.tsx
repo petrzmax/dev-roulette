69dc2f2cd6bfe8fd7cb51cdecafa06f73d254db4
Artur Petrzak <petrzmax@gmail.com>
2023-03-03 00:23:39 +0100

    Add basic bot script editor modal

---
+++ src/main/frontend/src/pages/bots/components/bot/components/scriptEditModal/ScriptEditorModal.tsx
@@ -0,0 +1,39 @@
+import CodeEditor from '@uiw/react-textarea-code-editor';
+import { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+export default function ScriptEditorModal(props: ScriptEditorModalProps) {
+  const [script, setScript] = useState(props.scriptBody);
+
+  return (
+    <Modal show={props.show} onHide={props.handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{props.name}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <CodeEditor
+          value={script}
+          onChange={(event) => setScript(event.target.value)}
+          language="js"
+          placeholder="This will be a brain of your bot"
+          data-color-mode="light"
+        ></CodeEditor>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.handleClose}>
+          Close
+        </Button>
+        <Button variant="success" onClick={props.handleClose}>
+          Save
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+interface ScriptEditorModalProps {
+  show: boolean;
+  name: string;
+  scriptBody: string;
+  handleClose: () => void;
+}
