6dbc0538ef19d7e9a5174741d52130c9fa898c2e
Artur Petrzak <petrzmax@gmail.com>
2023-03-16 22:26:44 +0100

    Move creating userDataDto to service

--- src/main/java/com/devroulette/restapi/user/restController/UserRestController.java
+++ src/main/java/com/devroulette/restapi/user/restController/UserRestController.java
@@ -1,10 +1,7 @@
 package com.devroulette.restapi.user.restController;
 
 import com.devroulette.restapi.common.constant.Endpoints;
-import com.devroulette.restapi.user.bots.dto.BotDto;
-import com.devroulette.restapi.user.bots.service.query.BotQueryService;
 import com.devroulette.restapi.user.dto.UserDataDto;
-import com.devroulette.restapi.user.entity.User;
 import com.devroulette.restapi.user.service.AuthenticatedUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,24 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(Endpoints.USER)
 @RequiredArgsConstructor
 public class UserRestController {
     private final AuthenticatedUserService authenticatedUserService;
-    private final BotQueryService botQueryService;
 
     @GetMapping()
     public ResponseEntity getUserData() {
-        User user = this.authenticatedUserService.getUser();
-        // TODO refactor, maybe move to above service?
-        String role = user.getRole();
-        long balance = user.getBalance();
-        List<BotDto> bots = this.botQueryService.getUserBots(user.getId());
-
-        // TODO Probably querydsl which will return dto
-        return new ResponseEntity(new UserDataDto(role, balance, bots), HttpStatus.OK);
+        UserDataDto userDataDto = this.authenticatedUserService.getUserDataDto();
+        return new ResponseEntity(userDataDto, HttpStatus.OK);
     }
 }
--- src/main/java/com/devroulette/restapi/user/service/AuthenticatedUserService.java
+++ src/main/java/com/devroulette/restapi/user/service/AuthenticatedUserService.java
@@ -1,6 +1,9 @@
 package com.devroulette.restapi.user.service;
 
 import com.devroulette.restapi.common.constant.ErrorMessages;
+import com.devroulette.restapi.user.bots.dto.BotDto;
+import com.devroulette.restapi.user.bots.service.query.BotQueryService;
+import com.devroulette.restapi.user.dto.UserDataDto;
 import com.devroulette.restapi.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -9,9 +12,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AuthenticatedUserService {
+    private final BotQueryService botQueryService;
 
     public User getUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -24,5 +30,15 @@ public class AuthenticatedUserService {
 
     public long getUserId() {
         return this.getUser().getId();
+    }
+
+    // TODO Maybe one query which will return whole dto?
+    public UserDataDto getUserDataDto() {
+        User user = this.getUser();
+        String role = user.getRole();
+        long balance = user.getBalance();
+        List<BotDto> bots = this.botQueryService.getUserBots(user.getId());
+
+        return new UserDataDto(role, balance, bots);
     }
 }
