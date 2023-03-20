e6153052f873c514241d357626a8ea62fd39612b
Artur Petrzak <petrzmax@gmail.com>
2023-03-20 22:06:22 +0100

    Set constant bot status badge width

--- src/main/frontend/src/pages/bots/components/bot/components/botStatusBadge/BotStatusBadge.tsx
+++ src/main/frontend/src/pages/bots/components/bot/components/botStatusBadge/BotStatusBadge.tsx
@@ -1,25 +1,38 @@
 import { ReactElement } from 'react';
 import { Badge } from 'react-bootstrap';
 import { BotStatus } from '../../../../../../common/constants';
+import css from './BotStatusBadge.module.scss';
 
 export default function BotStatusBadge(props: BotStatusBadgeProps) {
   function getBadge(): ReactElement {
     switch (props.status) {
       case BotStatus.READY:
-        return <Badge bg="warning">Ready</Badge>;
+        return (
+          <Badge className={css.badge} bg="warning">
+            Ready
+          </Badge>
+        );
 
       case BotStatus.RUNNING:
-        return <Badge bg="success">Running</Badge>;
+        return (
+          <Badge className={css.badge} bg="success">
+            Running
+          </Badge>
+        );
 
       case BotStatus.FAILED:
         return (
-          <Badge bg="danger" title={props.errorMessage}>
+          <Badge className={css.badge} bg="danger" title={props.errorMessage}>
             Failed
           </Badge>
         );
 
       default:
-        return <Badge bg="secondary">default</Badge>;
+        return (
+          <Badge className={css.badge} bg="secondary">
+            default
+          </Badge>
+        );
     }
   }
 
