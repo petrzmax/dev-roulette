45e4acec7e1d6f670a665284584a9a366e2f2972
Artur Petrzak <petrzmax@gmail.com>
2024-04-11 21:01:26 +0200

    Allow usage of h2-console

--- src/main/java/com/devroulette/restapi/spring/config/WebSecurityConfig.java
+++ src/main/java/com/devroulette/restapi/spring/config/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -26,8 +27,13 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         this.corsCustomizer.corsCustomizer(http);
 
-        http.authorizeRequests()
-                .requestMatchers(Endpoints.ROULETTE, Endpoints.EVENTS + "/**").permitAll()
+        // TODO rewrite to new approach, as this one is deprecated and marked for removal
+        //  https://stackoverflow.com/questions/43794721/spring-boot-h2-console-throws-403-with-spring-security-1-5-2
+        return http.csrf().ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**"))
+                .and()
+                .headers(headers -> headers.frameOptions().sameOrigin())
+                .authorizeRequests()
+                .requestMatchers(Endpoints.ROULETTE, Endpoints.EVENTS + "/**", "/h2-console/**").permitAll()
                 .requestMatchers(Endpoints.ADMIN + "/**").hasAuthority(Role.ADMIN)
                 .anyRequest().authenticated()
                 .and()
@@ -35,8 +41,7 @@ public class WebSecurityConfig {
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling((exceptions) -> exceptions
                         .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler()));
-
-        return http.build();
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
+                .build();
     }
 }
